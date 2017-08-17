021739ede1bfbe3da57e81bef2333bc3b2a5f782
thedavv <thedavv@users.noreply.github.com>
2017-08-17 13:24:12 +0200

    added files to test and changed absolute path

--- src/sk/racek/AdresaryStructurePrintoutTest.java
+++ src/sk/racek/AdresaryStructurePrintoutTest.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class AdresaryStructurePrintoutTest {
 	public static void main(String[] args) {
 		AdresaryStructurePrintout asp = new AdresaryStructurePrintout();
-		File testFile = new File(AdresaryStructurePrintout.currentFolderPath);
+		File testFile = new File(AdresaryStructurePrintout.currentFolderPath + "\\Test addr");
 		
 		asp.currentAddresaryPrintout(testFile, 0);
 	}
